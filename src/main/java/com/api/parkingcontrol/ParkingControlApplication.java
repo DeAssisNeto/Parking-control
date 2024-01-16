7b8e30526981f0a5eec40a1044001047e2b04b84
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-01-16 13:51:15 -0300

    Feat: Model, Dto, controller, repositories, and services created

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ParkingControlApplication {
+public class 	ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
